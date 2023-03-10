47dfa19b5cc6eab12e60cfc1be43412bddcfd99d
dmitrylobov <dilo1992.edu@gmail.com>
2023-03-10 18:39:33 +0300

    Initial commit

---
+++ src/main/java/by/lobov/rest_homework46/RestHomework46Application.java
@@ -0,0 +1,13 @@
+package by.lobov.rest_homework46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHomework46Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHomework46Application.class, args);
+	}
+
+}
---
+++ src/test/java/by/lobov/rest_homework46/RestHomework46ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lobov.rest_homework46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHomework46ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
